10c5e66152518afd6594b04debe467d550f6ac17
JSKIM <35033879+Mikusnuz@users.noreply.github.com>
2026-03-23 16:20:15 +0900

    fix: update SHA256 for Nudge v1.0.0

--- Casks/nudge.rb
+++ Casks/nudge.rb
@@ -1,6 +1,6 @@
 cask "nudge" do
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "b5f61ba7b8bd4c6b7676c86acd7212cb516b911e2b1fc4e21d45d437eca63942"
 
   url "https://github.com/mikusnuz/nudge/releases/download/v#{version}/Nudge.dmg"
   name "Nudge"
